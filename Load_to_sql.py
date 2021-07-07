ec5428337a16b4a3d84d288eb34c361767cbbfc1
Luise Higuera <luisehica@gmail.com>
2021-07-06 19:13:03 -0500

    starting NLP analysis

--- Load_to_sql.py
+++ Load_to_sql.py
@@ -1,18 +1,23 @@
 import psycopg2
 import csv
+import json
 import logging
 logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
+### Setup database
+with open('credentials.json', 'r') as json_file:
+    credentials = json.load(json_file)
 
 # connect with database
 logging.info("Connecting with DB")
-conn = psycopg2.connect("host=database-1.cj5pk51sbc9a.us-west-1.rds.amazonaws.com port=5432 dbname=Gacetas user=postgres password=inspiraCO2022")
+conn = psycopg2.connect(host=credentials['host'], port=credentials['port'], dbname=credentials['dbname'], user=credentials['user'], password=credentials['password'])
 cur = conn.cursor()
-
+# table = cur.execute("SELECT * FROM gacetas LIMIT 10;")
+# print(table)
 logging.info("Database Connected")
-
+"""
 logging.info('Starting to ingest gacetas table')
 with open('gacetas_clean.csv', 'r') as csv_file:
     reader = csv.reader(csv_file)
@@ -23,5 +28,6 @@ with open('gacetas_clean.csv', 'r') as csv_file:
             "INSERT INTO gacetas VALUES (%s, %s, %s, %s, %s, %s, %s)",
             (str(i), row[0], row[1], row[2], row[3], row[4], row[5])
         )
+"""
 logging.info('Ingest finished')
 conn.commit()
--- extract_text.py
+++ extract_text.py
@@ -1,25 +1,54 @@
+from ntpath import join
 from PyPDF2 import PdfFileReader
+from pdf2image import convert_from_path
+import pytesseract
+from PIL import Image
+import os
 
-def extract_information(pdf_path):
+import logging
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+def extract_first_page(gaceta, image_output, text_output):
+
+    logging.info("Get first image from pdf")
+    images_from_path = convert_from_path('gacetas/'+gaceta, output_file="image_pdf.jpeg")
+
+    image = images_from_path[0]
+    fname = 'image/'+ image_output
+    image.save(fname,'PNG')
+    logging.info("Get text from first page")
+    text = pytesseract.image_to_string(Image.open('image/'+image_output))
+    with open('text/'+text_output, 'w') as output:
+        output.write(text)
+
+def extract_number_pages(pdf_path):
     with open(pdf_path, 'rb') as f:
         pdf = PdfFileReader(f)
-        information = pdf.getDocumentInfo()
         number_of_pages = pdf.getNumPages()
     
-    txt = f"""
-    Information about: {pdf_path}
-
-    Author: {information.author}
-    Creator: {information.creator}
-    Producer: {information.producer}
-    Subject: {information.subject}
-    Title: {information.title}
-    Number of pages: {number_of_pages}
-    """
-    
-    print(txt)
-    return pdf
+    return number_of_pages
 
 if __name__=='__main__':
-    path = "./gacetas/gaceta_578.pdf"
-    extract_information(path)
+    gaceta_list = os.listdir('gacetas')
+    gaceta_first_page = os.listdir('image')
+
+    gaceta_corrected = []
+    for gaceta in gaceta_first_page:
+        split_gaceta = gaceta.split("_")
+        join_gaceta = split_gaceta[0]+"_"+split_gaceta[1]
+        gaceta_corrected.append(join_gaceta)
+
+    gaceta_restante = set(gaceta_list).difference(set(gaceta_corrected))
+    print(len(gaceta_list))
+    print(len(gaceta_first_page))
+    print(len(gaceta_restante))
+
+
+    for gaceta in gaceta_restante:
+        logging.info("Starting getting first page of "+gaceta)
+        image_output = gaceta+'_first_page.png'
+        text_output = gaceta+'first_page.txt'
+        extract_first_page(gaceta, image_output, text_output)
+        logging.info("Finishing text extraction")
